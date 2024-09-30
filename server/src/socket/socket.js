d1d674c36d875c5eb3d02d5dc9efcb3da27b912e
Shovan Sahu <shovansahu000@gmail.com>
2024-09-30 10:22:28 +0530

    .

--- server/src/socket/socket.js
+++ server/src/socket/socket.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://social-loop.vercel.app/",
+    origin: "https://socialloop-by-shovan.onrender.com",
     methods: ["GET", "POST"],
   },
 });
